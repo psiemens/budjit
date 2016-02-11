cf61e6ff414b02ebb4d0565340766cdb85be834b
Peter Siemens <peterjsiemens@gmail.com>
2016-02-10 16:24:37 -0800

    Render list of transactions on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var Transaction = require('../models/transaction');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return Transaction.findAsync({}).then(function(results){
+    return res.render('index', { transactions: results});
+  });
 });
 
 module.exports = router;
