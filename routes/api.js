44fd2d8420745d29b0a3642f0750d4ff98bd4ae2
Peter Siemens <peterjsiemens@gmail.com>
2016-02-10 20:50:02 -0800

    Add logic to confirm SMS alert subscription

--- routes/api.js
+++ routes/api.js
@@ -6,14 +6,36 @@ var Twilio = require('../helpers/twilio');
 
 // Twilio SMS Request URL
 router.post('/twilio/sms', function(req, res, next) {
-  return Transaction.create(Twilio.parseSMS(req.body)).then(function (object, err) {
-    if (err) {
-      res.send('There was an error recording the transaction.')
-    } else {
-      res.send(object);
-    }  
-  });
+  
+  // Two options:
+
+  // 1. Create a new transaction record from the SMS body
+  function createTransaction(data){
+    return Transaction.create(data).then(function (object, err) {
+      if (err) {
+        res.send('There was an error recording the transaction.')
+      } else {
+        res.send(object);
+      }  
+    });
+  }
+
+  // 2. Respond 'YES' to confirm the RBC Alert subscription
+  function confirmSubscription(){
+    res.set('Content-Type', 'text/xml');
+    res.send('<?xml version="1.0" encoding="UTF-8"?><Response><Message><Body>YES</Body></Message></Response>');
+  }
+
+  try {
+    var transactionData = Twilio.parseSMS(req.body);
+    return createTransaction(transactionData);
+  }
+  catch (e) {
+    return confirmSubscription();
+  }
 
 });
 
 module.exports = router;
+
+
